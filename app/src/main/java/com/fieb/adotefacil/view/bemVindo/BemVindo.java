6e0989772c41b9f905ca87ce3b9a4d1b6c2edfae
mariahalbquerque <mariah.victoria.spd@gmail.com>
2023-04-28 22:48:20 -0300

    commit

---
+++ app/src/main/java/com/fieb/adotefacil/view/bemVindo/BemVindo.java
@@ -0,0 +1,30 @@
+package com.fieb.adotefacil.view.bemVindo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.fieb.adotefacil.R;
+import com.fieb.adotefacil.view.bemVindo.principal.Principal;
+
+public class BemVindo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bem_vindo);
+
+        Button acesso = findViewById(R.id.btn_acesso);
+        acesso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BemVindo.this, Principal.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
