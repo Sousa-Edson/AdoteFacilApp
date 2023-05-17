2908f4005d5b2085b135478cb67a81a27c874afb
mariahalbquerque <mariah.victoria.spd@gmail.com>
2023-05-16 22:03:12 -0300

    criando login e melhorando a tela

--- app/src/main/java/com/fieb/adotefacil/view/login/Login.java
+++ app/src/main/java/com/fieb/adotefacil/view/login/Login.java
@@ -2,15 +2,48 @@ package com.fieb.adotefacil.view.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.fieb.adotefacil.R;
+import com.fieb.adotefacil.databinding.ActivityLoginBinding;
+import com.fieb.adotefacil.view.principal.Principal;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Login extends AppCompatActivity {
-
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnEntrar.setOnClickListener(view ->{
+            String email = binding.editEmail.getText().toString();
+            String senha = binding.editSenha.getText().toString();
+            if (email.isEmpty() || senha.isEmpty()) {
+                Snackbar snackBar = Snackbar.make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT);
+                snackBar.setBackgroundTint(Color.RED);
+                snackBar.show();
+            } else {
+                navegarTelaPrincipal();
+            }
+        });
+        binding.txtTelaCadastro.setOnClickListener(view ->{
+            navegatCadastro();
+        });
+    }
+
+    private void navegatCadastro() {
+        Toast.makeText(Login.this,"IR PARA TELA DE CADASTRO>>>",Toast.LENGTH_LONG).show();
+    }
+
+    private void navegarTelaPrincipal() {
+        Intent intent = new Intent(Login.this, Principal.class);
+        startActivity(intent);
+        finish();
     }
 }
