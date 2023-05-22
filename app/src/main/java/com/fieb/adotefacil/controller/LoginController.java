2510bf0a1768a89ddaf602689376b6a0fc144041
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-05-21 23:38:08 -0300

    salvando usuario no banco de dados - funcionado

--- app/src/main/java/com/fieb/adotefacil/controller/LoginController.java
+++ app/src/main/java/com/fieb/adotefacil/controller/LoginController.java
@@ -6,14 +6,18 @@ import com.fieb.adotefacil.api.ConexaoSqlServer;
 import com.fieb.adotefacil.model.LoginModel;
 
 import java.sql.PreparedStatement;
-
+import org.mindrot.jbcrypt.BCrypt;
 public class LoginController {
+//    private BCryptPasswordEncoder passwordEncoder
     public int cadastrarLogin(LoginModel loginModel , Context context){
         int resposta=0;
         try{
             PreparedStatement pst = ConexaoSqlServer.conectar(context).prepareStatement("insert into usuario (email,senha,ativo) values(?,?,?)");
             pst.setString(1,loginModel.getEmail());
-            pst.setString(2,loginModel.getSenha());
+            //  passwordEncoder.encode
+//            pst.setString(2,loginModel.getSenha());
+            pst.setString(2,BCrypt.hashpw(loginModel.getSenha(), BCrypt.gensalt()));
+
             pst.setString(3,"true");
             resposta= pst.executeUpdate();
         }catch (Exception e){
