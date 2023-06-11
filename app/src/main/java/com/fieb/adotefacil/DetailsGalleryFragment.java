5eccbd696e38cea6e149e670ebf0d46e67372878
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-06-11 18:36:34 -0300

    UPDATE: criado meio de exibir imagens em detalhe animal OK

--- app/src/main/java/com/fieb/adotefacil/DetailsGalleryFragment.java
+++ app/src/main/java/com/fieb/adotefacil/DetailsGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.fieb.adotefacil;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +23,7 @@ import com.fieb.adotefacil.databinding.FragmentDetailsEventBinding;
 import com.fieb.adotefacil.model.Animal;
 import com.fieb.adotefacil.model.Evento;
 import com.fieb.adotefacil.model.PetImagem;
+import com.fieb.adotefacil.util.ConverteData;
 import com.fieb.adotefacil.view.HomeFragment;
 
 import java.util.ArrayList;
@@ -29,62 +32,70 @@ import java.util.List;
 
 public class DetailsGalleryFragment extends Fragment {
     private FragmentDetailsAnimalBinding binding;
-    String teste="Edson";
     TextView txtAnimal, txtSexo, txtDescricao, txtObservacao,txtIdade;
     ImageView txtCaminhoFoto;
     RecyclerView recyclerView_Foto  ;
+    Animal animal = new Animal();
+
     public DetailsGalleryFragment(Animal animal) {
-        Bundle args = getArguments();
-        if (args != null) {
-            String valor = args.getString(animal.getNome());
-            System.out.println("AQUI :::: DetailsGalleryFragment ::: "+valor.toString());
-        }
+        this.animal = animal;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = FragmentDetailsAnimalBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-//        recebe();
-        preencheTela();
-        return root;
-    }
-
-    public void recebe(){
         txtAnimal=binding.nomeAnimal;
         txtDescricao=binding.descricaoAnimal;
         txtObservacao=binding.observacaoAnimal;
         txtCaminhoFoto=binding.fotoAnimalPrincipal;
         txtSexo=binding.sexoAnimal;
         txtIdade=binding.idadeAnimal;
-        Bundle args = getArguments();
-        if (args != null) {
-            txtAnimal.setText(args.getString("txtAnimal"));
-            txtDescricao.setText(args.getString("txtResumo"));
-            txtObservacao.setText(args.getString("txtObservacao"));
-            txtSexo.setText(args.getString("txtSexo"));
-            txtIdade.setText(args.getString("txtIdade"));
-            String imageUrl=args.getString("txtFoto");
+        recyclerView_Foto = binding.recyclerViewFoto;
+        carregaTela();
+//        recebe();
+//        preencheTela();
+        return root;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void carregaTela (){
+int idAnimal=0;
+        if (animal != null) {
+            txtAnimal.setText(animal.getNome().toString());
+            txtDescricao.setText(animal.getResumo().toString());
+            txtObservacao.setText(animal.getObservacao().toString());
+            txtSexo.setText(animal.getSexo()==2 ?"Macho":animal.getSexo() ==1 ?"Femea":"Sexo Indefinido");
+            ConverteData converteData = new ConverteData();
+            txtIdade.setText( converteData.calculaIdade(animal.getNascimento()));
+            idAnimal= animal.getId();
+            String imageUrl=null;
+            try {
+                imageUrl= animal.getCaminhoFotoAnimal().get(0).getCaminhoImagem().toString();
+            }catch (Exception e){
+                System.out.println("ERRO AO CARREGAR IMAGEM: "+e.getMessage());
+                imageUrl="https://res.cloudinary.com/daieys39b/image/upload/v1686251907/postagem/sem-imagem_owc6zt.jpg";
+            }
+
             Glide.with(this.getContext()).load(imageUrl).into(this.txtCaminhoFoto);
+
+            // configurar o recyclerView
+            recyclerView_Foto.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+            recyclerView_Foto.setHasFixedSize(true);
+            //configurar o adapter
+            List<PetImagem> listaImagem = new ArrayList<>();
+            AnimalController animalController = new AnimalController();
+            listaImagem = animalController.listaImagem(DetailsGalleryFragment.this.getContext(),String.valueOf(idAnimal));
+
+            AdapterAnimalFoto adapterAnimalFoto = new AdapterAnimalFoto(DetailsGalleryFragment.this, listaImagem);
+            recyclerView_Foto.setAdapter(adapterAnimalFoto);
+
+
+            System.out.println("EXIBINDO EM DETAILSGALLERYFRAGMENT _-_ "+listaImagem.get(0).getCaminhoImagem());
         }
     }
-    public void preencheTela() {
-// configurar o recyclerView
-//        RecyclerView recyclerView_Foto = binding.recyclerViewFoto;
-          recyclerView_Foto = binding.recyclerViewFoto;
-        recyclerView_Foto.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        recyclerView_Foto.setHasFixedSize(true);
-//configurar o adapter
-        List<PetImagem> listaImagem = new ArrayList<>();
-        AnimalController animalController = new AnimalController();
-        listaImagem = animalController.listaImagem(DetailsGalleryFragment.this.getContext(),"1");
-
-        AdapterAnimalFoto adapterAnimalFoto = new AdapterAnimalFoto(DetailsGalleryFragment.this, listaImagem);
-        recyclerView_Foto.setAdapter(adapterAnimalFoto);
 
 
-        System.out.println("EXIBINDO EM DETAILSGALLERYFRAGMENT _-_ "+listaImagem.get(0).getCaminhoImagem());
-    }
 }
--- app/src/main/java/com/fieb/adotefacil/adapter/AdapterAnimalFoto.java
+++ app/src/main/java/com/fieb/adotefacil/adapter/AdapterAnimalFoto.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,7 +14,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.fieb.adotefacil.DetailsGalleryFragment;
 import com.fieb.adotefacil.R;
+import com.fieb.adotefacil.model.Evento;
 import com.fieb.adotefacil.model.PetImagem;
+import com.fieb.adotefacil.view.GalleryFragment;
+import com.fieb.adotefacil.view.login_cadastro.Cadastro;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,50 +25,45 @@ import java.util.List;
 public class AdapterAnimalFoto extends RecyclerView.Adapter<AdapterAnimalFoto.FotoViewHolder> {
     private List<PetImagem> petImagems;
     private ArrayList<Uri> fotos;
-//    public View.OnClickListener listener;
     public AdapterAnimalFoto(DetailsGalleryFragment detailsGalleryFragment, List<PetImagem> petImagems) {
         System.out.println("AQUI EM ADAPTER : "+petImagems.get(0).getCaminhoImagem());
+        this.petImagems = petImagems;
+    }
 
-       this.petImagems = petImagems;
-   }
+
     @NonNull
     @Override
     public AdapterAnimalFoto.FotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        System.out.println("AQUI EM ADAPTER 2: "+petImagems.get(0).getCaminhoImagem());
         View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.animal_foto_item,parent,false);
-        System.out.println("AQUIIIIIIIIIIIIIIIIIIIIIIIII");
         return new FotoViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterAnimalFoto.FotoViewHolder holder, int position) {
-        System.out.println("ADAPTER HOLDER : ::");
-//        PetImagem fotos = animais.get(position);
-        String imageUrl= petImagems.get(position).getCaminhoImagem().toString();
-        System.out.println("ADAPTER HOLDER : "+imageUrl);
+        System.out.println("AQUI EM ADAPTER 3 : "+petImagems.get(position).getCaminhoImagem());
+        System.out.println("VENDO O TAMANHO DE UMA FORMA: "+petImagems.size());
+
+        PetImagem fotos = petImagems.get(position);
+        String imageUrl= fotos.getCaminhoImagem();
+//        imageUrl="https://res.cloudinary.com/daieys39b/image/upload/v1686251907/postagem/sem-imagem_owc6zt.jpg";
         Glide.with(holder.itemView.getContext()).load(imageUrl).into(holder.foto);
-
-            // Define o listener de clique para o item
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Toast.makeText(v.getContext(), "Clicou em: ALGUMA COISA " , Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-
+        holder.texto.setText("-_-");
+    }
 
     @Override
     public int getItemCount() {
+        System.out.println("TAMANHO: "+petImagems.size());
         return petImagems.size();
     }
+
     public static class FotoViewHolder extends RecyclerView.ViewHolder {
         ImageView foto = itemView.findViewById(R.id.fotoAnimalBloco);
+        TextView texto = itemView.findViewById(R.id.textoAnimalBloco);
 
         public FotoViewHolder(@NonNull View itemView) {
             super(itemView);
         }
-
-
     }
+
 }
