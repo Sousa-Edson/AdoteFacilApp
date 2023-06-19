f3444fc259ac95ecf9614cf17f77485022dfca72
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-06-19 08:00:13 -0300

    BUGFIX:corrigindo salvar permissão usuario

--- app/src/main/java/com/fieb/adotefacil/controller/LoginController.java
+++ app/src/main/java/com/fieb/adotefacil/controller/LoginController.java
@@ -14,11 +14,12 @@ public class LoginController {
     public int cadastrarLogin(LoginModel loginModel, Context context) {
         int resposta = 0;
         try {
-            PreparedStatement pst = ConexaoSqlServer.conectar(context).prepareStatement("insert into usuario (email,senha,ativo) values(?,?,?)");
+            PreparedStatement pst = ConexaoSqlServer.conectar(context).prepareStatement("insert into usuario (email,senha,ativo,permissao) values(?,?,?,?)");
             pst.setString(1, loginModel.getEmail());
             pst.setString(2, BCrypt.hashpw(loginModel.getSenha(), BCrypt.gensalt()));
 
             pst.setString(3, "true");
+            pst.setString(4, "USUARIO");
             resposta = pst.executeUpdate();
         } catch (Exception e) {
             e.getMessage();
